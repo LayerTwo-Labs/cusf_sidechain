3f3831d1f403aa0803aa1b57d8593e4f221c2732
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2024-09-03 11:27:55 -0400

    Implement Archive::connect

--- src/state/archive.rs
+++ src/state/archive.rs
@@ -1,3 +1,4 @@
+use bitcoin::block;
 use heed::{types::*, Env};
 use heed::{Database, RwTxn};
 use miette::{IntoDiagnostic, Result};
@@ -31,10 +32,35 @@ impl Archive {
     pub fn connect(
         &self,
         txn: &mut RwTxn,
-        header: &Header,
+        header: Header,
         transactions: &[Transaction],
     ) -> Result<()> {
-        todo!();
+        let last_transaction = self.transactions.last(txn).into_diagnostic()?;
+        let mut transaction_number = match last_transaction {
+            Some((transaction_number, _transaction)) => transaction_number + 1,
+            None => 0,
+        };
+        let transaction_range_start = transaction_number;
+        for transaction in transactions {
+            self.transactions
+                .put(txn, &transaction_number, transaction)
+                .into_diagnostic()?;
+            transaction_number += 1;
+        }
+        let transaction_range_end = transaction_number;
+        let last_header = self.headers.last(txn).into_diagnostic()?;
+        let block_number = match last_header {
+            Some((block_number, _header)) => block_number + 1,
+            None => 0,
+        };
+        self.headers
+            .put(
+                txn,
+                &block_number,
+                &(header, (transaction_range_start, transaction_range_end)),
+            )
+            .into_diagnostic()?;
+        Ok(())
     }
 
     // Disconnect number latest blocks.
